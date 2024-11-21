8ac1e5882a4003d365df8266da48ba10a1df34cc
Opetunde <dhorllyibi@gmail.com>
2024-11-21 04:43:16 -0500

    flaskproject

--- app.py
+++ app.py
@@ -15,8 +15,34 @@ class Todo(db.Model):
         return '<Task %r>' % self.id
 
 
-@app.route('/', methods=['POST', 'GET'])
-def index():
+@app.route('/landing')
+def landing():
+    return render_template('landing.html')  # Render the landing page
+
+@app.route('/task-master')
+def task_master():
+    return redirect('/')  # Redirect to the Task Master project (your current index route)
+
+
+# @app.route('/', methods=['POST', 'GET'])
+# def index():
+#     if request.method == 'POST':
+#         task_content = request.form['content']
+#         new_task = Todo(content=task_content)
+
+#         try:
+#             db.session.add(new_task)
+#             db.session.commit()
+#             return redirect('/')
+#         except:
+#             return 'There was an issue adding your task'
+
+#     else:
+#         tasks = Todo.query.order_by(Todo.date_created).all()
+#         return render_template('index.html', tasks=tasks)
+
+@app.route('/assignment/', methods=['POST', 'GET'])
+def assignment_index():
     if request.method == 'POST':
         task_content = request.form['content']
         new_task = Todo(content=task_content)
@@ -24,28 +50,29 @@ def index():
         try:
             db.session.add(new_task)
             db.session.commit()
-            return redirect('/')
+            return redirect('/assignment/')
         except:
             return 'There was an issue adding your task'
 
     else:
         tasks = Todo.query.order_by(Todo.date_created).all()
-        return render_template('index.html', tasks=tasks)
+        return render_template('assignment/index.html', tasks=tasks)
 
 
-@app.route('/delete/<int:id>')
-def delete(id):
+@app.route('/assignment/delete/<int:id>')
+def assignment_delete(id):
     task_to_delete = Todo.query.get_or_404(id)
 
     try:
         db.session.delete(task_to_delete)
         db.session.commit()
-        return redirect('/')
+        return redirect('/assignment/')
     except:
         return 'There was a problem deleting that task'
 
-@app.route('/update/<int:id>', methods=['GET', 'POST'])
-def update(id):
+
+@app.route('/assignment/update/<int:id>', methods=['GET', 'POST'])
+def assignment_update(id):
     task = Todo.query.get_or_404(id)
 
     if request.method == 'POST':
@@ -53,12 +80,52 @@ def update(id):
 
         try:
             db.session.commit()
-            return redirect('/')
+            return redirect('/assignment/')
         except:
             return 'There was an issue updating your task'
 
     else:
-        return render_template('update.html', task=task)
+        return render_template('assignment/update.html', task=task)
+
+
+# @app.route('/delete/<int:id>')
+# def delete(id):
+#     task_to_delete = Todo.query.get_or_404(id)
+
+#     try:
+#         db.session.delete(task_to_delete)
+#         db.session.commit()
+#         return redirect('/')
+#     except:
+#         return 'There was a problem deleting that task'
+
+# @app.route('/assignment/delete/<int:id>')
+# def assignment_delete(id):
+#     task_to_delete = Todo.query.get_or_404(id)
+
+#     try:
+#         db.session.delete(task_to_delete)
+#         db.session.commit()
+#         return redirect('/assignment/')
+#     except:
+#         return 'There was a problem deleting that task'
+
+
+# @app.route('/update/<int:id>', methods=['GET', 'POST'])
+# def update(id):
+#     task = Todo.query.get_or_404(id)
+
+#     if request.method == 'POST':
+#         task.content = request.form['content']
+
+#         try:
+#             db.session.commit()
+#             return redirect('/')
+#         except:
+#             return 'There was an issue updating your task'
+
+#     else:
+#         return render_template('update.html', task=task)
 
 
 if __name__ == "__main__":
